5f28c71cafc58222130b44433d5ea9616dfb770a
Olympic <olympic@Olympic.local>
2025-05-26 15:40:59 -0500

    Added blockquote formatting to notes. Added May notes on misc topics

--- notes/md2html.sh
+++ notes/md2html.sh
@@ -4,6 +4,8 @@ for file in 2025/*.md
 do
     hfile=`echo $file | sed 's/\.md$/.html/'`
     echo "Converting '$file' --> '$hfile'"
-    markdown $file > $hfile
+    markdown $file > tmp.html
+    cat notes-css.html tmp.html > $hfile
+    rm tmp.html
 done
 
